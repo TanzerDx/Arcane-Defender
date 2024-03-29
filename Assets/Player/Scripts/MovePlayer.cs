fbdfd6d6f5cca0ce0b03a16efa2930fb7052cdca
Maxence Moncel <maxence.moncel@epita.fr>
2024-03-29 19:12:35 +0100

    feat: Pushed this branch + Added animations to the player character. Enemies animations have been created

--- Assets/Player/Scripts/MovePlayer.cs
+++ Assets/Player/Scripts/MovePlayer.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class MovePlayer : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] private Animator PlayerAnim;
+
+    [SerializeField] private SpriteRenderer sprite;
 
     void Update()
     {
@@ -15,6 +18,17 @@ public class MovePlayer : MonoBehaviour
     {
         float xValue = (Input.GetAxis("Horizontal")) * moveSpeed  * Time.deltaTime;
         float yValue = (Input.GetAxis("Vertical")) * moveSpeed  * Time.deltaTime;
+        
+        PlayerAnim.SetBool("IsMoving", xValue != 0 || yValue != 0);
+
+        if (xValue > 0)
+        {
+            sprite.flipX = true;
+        }
+        else if (xValue < 0)
+        {
+            sprite.flipX = false;
+        }
 
         transform.Translate(xValue, yValue , 0, Space.World);
     }
