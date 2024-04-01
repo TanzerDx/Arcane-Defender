9752769e96d0bdaf9161281e22d40cfbf086520c
Maxence Moncel <maxence.moncel@epita.fr>
2024-04-01 13:00:33 +0200

    feat: added some fields and patched methods in the Stats class + big modifications ahead

--- Assets/Enemy/Scripts/Stats.cs
+++ Assets/Enemy/Scripts/Stats.cs
@@ -6,24 +6,62 @@ using UnityEngine;
 public class Stats
 {
     //Base stats
+    [Tooltip("The current health of the monster")]
     private int health;
+    [Tooltip("The move speed of the monster (1 being the base speed)")]
     private float moveSpeed;
+    public float MoveSpeed
+    {
+        get { return moveSpeed; }
+    }
+    
+    [Tooltip("The base speed of the enemy")]
+    private float speedSaver;
+    [Tooltip("The amount of damage dealt to the player")]
     private int damage;
+    public int Damage
+    {
+        get { return damage; }
+    }
     
     //Damage resistances
+    [Tooltip("The proportion of physical damage taken (1 = No resistance || 0 = Immunity)")]
     private float physResistance;
+    [Tooltip("The proportion of magical damage taken (1 = No resistance || 0 = Immunity)")]
     private float magicResistance;
     
     //Crowd Control resistances
+    [Tooltip("The proportion of stun duration applied (1 = No resistance || 0 = Immunity)")]
     private float stunResist;
+    [Tooltip("The proportion of slow duration applied (1 = No resistance || 0 = Immunity)")]
     private float slowResist;
+    [Tooltip("The proportion of poison duration applied (1 = No resistance || 0 = Immunity)")]
     private float poisonResist;
+    
+    [Tooltip("Is this monster currently stunned?")]
     private bool isStunned;
+    public bool IsStunned
+    {
+        get { return isStunned; }
+    }
+    [Tooltip("Is this monster currently slowed?")]
     private bool isSlowed;
+    public bool IsSlowed
+    {
+        get { return isSlowed; }
+    }
+    [Tooltip("Is this monster currently poisoned?")]
     private bool isPoisoned;
+
+    private int poisonPotency;
+    public bool IsPoisoned
+    {
+        get { return isPoisoned; }
+    }
     
     //Other & miscellaneous
-    private float damageMultiplyer;
+    [Tooltip("The damage multiplier of this enemy. 1 Is the default value (no damage modification)")]
+    private float damageMultiplier;
 
 
 
@@ -32,60 +70,71 @@ public class Stats
     {
         health = _health;
         moveSpeed = _moveSpeed;
+        speedSaver = moveSpeed;
         damage = _damage;
+        
         physResistance = _physResistance;
         magicResistance = _magicResistance;
         stunResist = _stunResist;
         slowResist = _slowResist;
         poisonResist = _poisonResist;
-        damageMultiplyer = 1f;
-    }
+        
+        damageMultiplier = 1f;
 
-    public int GetDamage()
-    {
-        return damage;
+        isStunned = false;
+        isSlowed = false;
+        isPoisoned = false;
     }
+    
+    
 
     public int TakingDamage(int amount, bool physical)
     {
         if (physical)
         {
-            health = health - (int)(amount * physResistance*damageMultiplyer);
+            health = health - (int)(amount * physResistance*damageMultiplier);
         }
         else
         {
-            health = health - (int)(amount * magicResistance * damageMultiplyer);
+            health = health - (int)(amount * magicResistance * damageMultiplier);
         }
 
         return health;
     }
+    
+    
 
     public float GetStunned(float duration)
     {
         isStunned = true;
+        moveSpeed = 0;
         return duration * stunResist;
     }
     
-    public float GetSlowed(float duration)
+    public float GetSlowed(float duration, float intensity)
     {
         isSlowed = true;
+        moveSpeed *= intensity;
         return duration * slowResist;
     }
     
-    public float GetPoisoned(float duration)
+    public float GetPoisoned(float duration, int potency)
     {
         isPoisoned = true;
+        poisonPotency = potency;
         return duration * poisonResist;
     }
 
     public void EndStun()
     {
         isStunned = false;
+        moveSpeed = speedSaver;
     }
     
     public void EndSlow()
     {
         isSlowed = false;
+        moveSpeed = speedSaver;
     }
     
     public void EndPoison()
